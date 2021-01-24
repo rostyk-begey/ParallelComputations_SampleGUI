2642dccfa2758ac00eadf4e5e3f0191c2f6c8e50
Alexandr Shkurinskiy <ashkurinskiy@gmail.com>
2021-01-24 18:19:59 +0200

    Add project files.

---
+++ forms/Form1.Designer.cs
@@ -0,0 +1,217 @@
+﻿namespace forms
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonL1 = new System.Windows.Forms.Button();
+            this.buttonL2 = new System.Windows.Forms.Button();
+            this.buttonL3 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonR3 = new System.Windows.Forms.Button();
+            this.buttonR2 = new System.Windows.Forms.Button();
+            this.buttonR1 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.buttonC3 = new System.Windows.Forms.Button();
+            this.buttonC2 = new System.Windows.Forms.Button();
+            this.buttonC1 = new System.Windows.Forms.Button();
+            this.labelC1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // buttonL1
+            // 
+            this.buttonL1.Location = new System.Drawing.Point(380, 49);
+            this.buttonL1.Name = "buttonL1";
+            this.buttonL1.Size = new System.Drawing.Size(75, 23);
+            this.buttonL1.TabIndex = 5;
+            this.buttonL1.Text = "play";
+            this.buttonL1.UseVisualStyleBackColor = true;
+            this.buttonL1.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // buttonL2
+            // 
+            this.buttonL2.Location = new System.Drawing.Point(461, 49);
+            this.buttonL2.Name = "buttonL2";
+            this.buttonL2.Size = new System.Drawing.Size(75, 23);
+            this.buttonL2.TabIndex = 6;
+            this.buttonL2.Text = "pause";
+            this.buttonL2.UseVisualStyleBackColor = true;
+            this.buttonL2.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // buttonL3
+            // 
+            this.buttonL3.Location = new System.Drawing.Point(542, 49);
+            this.buttonL3.Name = "buttonL3";
+            this.buttonL3.Size = new System.Drawing.Size(75, 23);
+            this.buttonL3.TabIndex = 7;
+            this.buttonL3.Text = "stop";
+            this.buttonL3.UseVisualStyleBackColor = true;
+            this.buttonL3.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(476, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 24);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Line";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(791, 21);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(95, 24);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Rectangle";
+            // 
+            // buttonR3
+            // 
+            this.buttonR3.Location = new System.Drawing.Point(876, 49);
+            this.buttonR3.Name = "buttonR3";
+            this.buttonR3.Size = new System.Drawing.Size(75, 23);
+            this.buttonR3.TabIndex = 12;
+            this.buttonR3.Text = "stop";
+            this.buttonR3.UseVisualStyleBackColor = true;
+            this.buttonR3.Click += new System.EventHandler(this.buttonR3_Click);
+            // 
+            // buttonR2
+            // 
+            this.buttonR2.Location = new System.Drawing.Point(795, 49);
+            this.buttonR2.Name = "buttonR2";
+            this.buttonR2.Size = new System.Drawing.Size(75, 23);
+            this.buttonR2.TabIndex = 11;
+            this.buttonR2.Text = "pause";
+            this.buttonR2.UseVisualStyleBackColor = true;
+            this.buttonR2.Click += new System.EventHandler(this.buttonR2_Click);
+            // 
+            // buttonR1
+            // 
+            this.buttonR1.Location = new System.Drawing.Point(714, 49);
+            this.buttonR1.Name = "buttonR1";
+            this.buttonR1.Size = new System.Drawing.Size(75, 23);
+            this.buttonR1.TabIndex = 10;
+            this.buttonR1.Text = "play";
+            this.buttonR1.UseVisualStyleBackColor = true;
+            this.buttonR1.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(120, 22);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(77, 24);
+            this.label3.TabIndex = 16;
+            this.label3.Text = "Counter";
+            // 
+            // buttonC3
+            // 
+            this.buttonC3.Location = new System.Drawing.Point(203, 49);
+            this.buttonC3.Name = "buttonC3";
+            this.buttonC3.Size = new System.Drawing.Size(75, 23);
+            this.buttonC3.TabIndex = 15;
+            this.buttonC3.Text = "stop";
+            this.buttonC3.UseVisualStyleBackColor = true;
+            this.buttonC3.Click += new System.EventHandler(this.button4_Click_1);
+            // 
+            // buttonC2
+            // 
+            this.buttonC2.Location = new System.Drawing.Point(122, 49);
+            this.buttonC2.Name = "buttonC2";
+            this.buttonC2.Size = new System.Drawing.Size(75, 23);
+            this.buttonC2.TabIndex = 14;
+            this.buttonC2.Text = "pause";
+            this.buttonC2.UseVisualStyleBackColor = true;
+            this.buttonC2.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // buttonC1
+            // 
+            this.buttonC1.Location = new System.Drawing.Point(41, 49);
+            this.buttonC1.Name = "buttonC1";
+            this.buttonC1.Size = new System.Drawing.Size(75, 23);
+            this.buttonC1.TabIndex = 13;
+            this.buttonC1.Text = "play";
+            this.buttonC1.UseVisualStyleBackColor = true;
+            this.buttonC1.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // labelC1
+            // 
+            this.labelC1.AutoSize = true;
+            this.labelC1.Font = new System.Drawing.Font("Microsoft Sans Serif", 72F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.labelC1.Location = new System.Drawing.Point(87, 143);
+            this.labelC1.Name = "labelC1";
+            this.labelC1.Size = new System.Drawing.Size(98, 108);
+            this.labelC1.TabIndex = 17;
+            this.labelC1.Text = "0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(990, 450);
+            this.Controls.Add(this.labelC1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.buttonC3);
+            this.Controls.Add(this.buttonC2);
+            this.Controls.Add(this.buttonC1);
+            this.Controls.Add(this.buttonR3);
+            this.Controls.Add(this.buttonR2);
+            this.Controls.Add(this.buttonR1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.buttonL3);
+            this.Controls.Add(this.buttonL2);
+            this.Controls.Add(this.buttonL1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button buttonL1;
+        private System.Windows.Forms.Button buttonL2;
+        private System.Windows.Forms.Button buttonL3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonR3;
+        private System.Windows.Forms.Button buttonR2;
+        private System.Windows.Forms.Button buttonR1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Button buttonC3;
+        private System.Windows.Forms.Button buttonC2;
+        private System.Windows.Forms.Button buttonC1;
+        private System.Windows.Forms.Label labelC1;
+    }
+}
+
---
+++ forms/Form1.cs
@@ -0,0 +1,338 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Windows.Forms;
+
+namespace forms
+{
+    public partial class Form1 : Form
+    {
+        EventWaitHandle ew;
+        EventWaitHandle ewC; // counter
+        EventWaitHandle ewLR; // Line right
+        EventWaitHandle ewLL; // Line left
+        EventWaitHandle ewRT; // Rectangle top
+        EventWaitHandle ewRR; // Rectangle right
+        EventWaitHandle ewRD; // Rectangle down
+        EventWaitHandle ewRL; // Rectangle left
+        bool Stop, Pause;
+        bool StopC, PauseC;
+        bool StopL, PauseL;
+        bool StopR, PauseR;
+        Pen pn = new Pen(Color.Red, 4);      
+        Pen pnLR = new Pen(Color.Blue, 10);  
+        Pen pnLL = new Pen(Color.Blue, 10);  
+        Pen pnRT = new Pen(Color.Blue, 10);  
+        Pen pnRR = new Pen(Color.Blue, 10);  
+        Pen pnRD = new Pen(Color.Blue, 10);  
+        Pen pnRL = new Pen(Color.Blue, 10);
+        Thread[] thL = new Thread[2];
+        Thread[] thR = new Thread[4];
+
+
+        Graphics g;
+        Graphics gLL;
+        Graphics gLR;
+        Graphics gRT;
+        Graphics gRR;
+        Graphics gRD;
+        Graphics gRL;
+        int XR = 760;       // X rect   Top-left point
+        int YR = 150;       // Y rect
+        int Rlen = 150;     // Length of side rect
+        int XL = 500;       // Betvin X Line
+        int YL = 200;       // betwin Y Line
+        int XLR;            // X right Line
+        int XLL;            // X left Line
+        int XRTR;           // X top-right
+        int YRRD;           // Y right-down
+        int XRDL;           // X down-left
+        int YRLT;           // Y left-top
+
+        public Form1()
+        {
+
+            InitializeComponent();
+            ew = new EventWaitHandle(false, EventResetMode.AutoReset);
+            ewC = new EventWaitHandle(false, EventResetMode.AutoReset);
+            ewLR = new EventWaitHandle(false, EventResetMode.AutoReset);
+            ewLL = new EventWaitHandle(false, EventResetMode.AutoReset);
+            ewRT = new EventWaitHandle(false, EventResetMode.AutoReset);
+            ewRR = new EventWaitHandle(false, EventResetMode.AutoReset);
+            ewRD = new EventWaitHandle(false, EventResetMode.AutoReset);
+            ewRL = new EventWaitHandle(false, EventResetMode.AutoReset);
+            Stop = true;
+            StopC = true;
+            StopL = true;
+            StopR = true;
+            XLR = XL;
+            XLL = XL;
+
+            XRTR = XR;
+            YRRD = YR;
+            XRDL = XR + Rlen;
+            YRLT = YR + Rlen;
+
+            g = CreateGraphics();
+            gLL = CreateGraphics();
+            gLR = CreateGraphics();
+            gRT = CreateGraphics();
+            gRR = CreateGraphics();
+            gRD = CreateGraphics();
+            gRL = CreateGraphics();
+
+        }
+
+        void ThreadMethod(int value)
+        {
+            Action<int> act = new Action<int>((v) =>
+            {
+                labelC1.Text = v.ToString();
+            });
+
+            int i = 0;
+            while (!StopC && i <= value)
+            {
+                if (PauseC)
+                {
+                    ewC.WaitOne();
+                    PauseC = false;
+                }
+                i += 1;
+                if (labelC1.InvokeRequired)
+                    labelC1.Invoke(new MethodInvoker(() =>
+                    {
+                        act(i);
+                    }));
+                else
+                    act(i);
+                Thread.Sleep(500);
+            }
+        }
+
+        private void button9_Click(object sender, EventArgs e) // Play Counter
+        {
+            if (StopC)
+            {
+                buttonC2.Enabled = buttonC3.Enabled = true;
+                buttonC1.Enabled = false;
+                StopC = false;
+                ThreadPool.QueueUserWorkItem(new WaitCallback((s) =>
+                {
+                    ThreadMethod(100);
+                }));
+            }
+            else
+            {
+                ewC.Set();
+                buttonC3.Enabled = true;
+            }
+            buttonC1.Enabled = false;
+        }
+
+        private void button8_Click(object sender, EventArgs e) // Pause Counter
+        {
+            PauseC = true;
+            buttonC3.Enabled = false;
+            buttonC1.Enabled = true;
+        }
+
+        private void button4_Click_1(object sender, EventArgs e) // Stop Counter
+        {
+            StopC = true;
+            buttonC2.Enabled = false;
+            buttonC1.Enabled = true;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e) // Play Rectangle
+        {
+            if (StopR)
+            {
+                buttonR2.Enabled = buttonR3.Enabled = true;
+                buttonR1.Enabled = false;
+                StopR = false;
+
+                //Top
+                thR[0] = new Thread(() =>
+                {
+                    while (!StopR && XRTR <= XR + Rlen)
+                    {
+                        if (PauseR)
+                        {
+                            ewRT.WaitOne();
+                            PauseR = false;
+                        }
+                        Thread.Sleep(50);
+
+                        gRT.DrawLine(pnRT, new Point(XR, YR), new Point(XRTR, YR));
+                        XRTR++;
+
+                    }
+                });
+
+                //right
+                thR[1] = new Thread(() =>
+                {
+                    while (!StopR && YRRD <= YR + Rlen)
+                    {
+                        if (PauseR)
+                        {
+                            ewRR.WaitOne();
+                            PauseR = false;
+                        }
+                        Thread.Sleep(50);
+
+                        gRR.DrawLine(pnRR, new Point(XR + Rlen, YR), new Point(XR + Rlen, YRRD));
+                        YRRD++;
+
+                    }
+                });
+
+                //down
+                thR[2] = new Thread(() =>
+                {
+                    while (!StopR && XRDL >= XR)
+                    {
+                        if (PauseR)
+                        {
+                            ewRD.WaitOne();
+                            PauseR = false;
+                        }
+                        Thread.Sleep(50);
+
+                        gRD.DrawLine(pnRD, new Point(XR + Rlen, YR + Rlen), new Point(XRDL, YR + Rlen));
+                        XRDL--;
+
+                    }
+                });
+
+                //left
+                thR[3] = new Thread(() =>
+                {
+                    while (!StopR && YRLT >= YR)
+                    {
+                        if (PauseR)
+                        {
+                            ewRL.WaitOne();
+                            PauseR = false;
+                        }
+                        Thread.Sleep(50);
+
+                        gRL.DrawLine(pnRL, new Point(XR, YR + Rlen), new Point(XR, YRLT));
+                        YRLT--;
+
+                    }
+                });
+
+                foreach (var item in thR)
+                {
+                    item.Start();
+                }
+            }
+            else
+            {
+                ewRT.Set();
+                ewRR.Set();
+                ewRD.Set();
+                ewRL.Set();
+                buttonL3.Enabled = true;
+            }
+            buttonL1.Enabled = false;
+        }
+
+        private void buttonR2_Click(object sender, EventArgs e) // Rectangle Pause
+        {
+            PauseR = true;
+            buttonR3.Enabled = false;
+            buttonR1.Enabled = true;
+        }
+
+        private void buttonR3_Click(object sender, EventArgs e) // Rectangle Stop
+        {
+            StopR = true;
+            buttonR2.Enabled = false;
+            buttonR1.Enabled = true;
+        }
+
+
+        private void button5_Click(object sender, EventArgs e)// Play Line
+        {
+            if (StopL)
+            {
+                buttonL2.Enabled = buttonL3.Enabled = true;
+                buttonL1.Enabled = false;
+                StopL = false;
+
+                //Right
+                thL[0] = new Thread(() =>
+                {
+                    while (!StopL && XLR <= XL + 200)
+                    {
+                        if (PauseL)
+                        {
+                            ewLR.WaitOne();
+                            PauseL = false;
+                        }
+                        Thread.Sleep(50);
+
+                        gLR.DrawLine(pnLR, new Point(XL, YL), new Point(XLR, YL));
+                        XLR++;
+                    
+                    }
+                });
+
+                //Left
+                thL[1] = new Thread(() =>
+                {
+                    while (!StopL && XLL >= XL - 200)
+                    {
+                        if (PauseL)
+                        {
+                            ewLL.WaitOne();
+                            PauseL = false;
+                        }
+                        Thread.Sleep(50);
+
+                        gLL.DrawLine(pnLL, new Point(XLL, YL) , new Point(XL, YL));
+                        XLL--;
+
+                    }
+                });
+
+                foreach (var item in thL)
+                {
+                    item.Start();
+                }
+            }
+            else
+            {
+                ewLL.Set();
+                ewLR.Set();
+                buttonL3.Enabled = true;
+            }
+            buttonL1.Enabled = false;
+        }
+
+        private void button6_Click(object sender, EventArgs e) // Pause Line
+        {
+            PauseL = true;
+            buttonL3.Enabled = false;
+            buttonL1.Enabled = true;
+        }
+
+        private void button7_Click(object sender, EventArgs e) // Stop Line
+        {
+            StopL = true;
+            buttonL2.Enabled = false;
+            buttonL1.Enabled = true;
+        }
+
+    }
+}
